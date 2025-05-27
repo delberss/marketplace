c256d4023c35fcc29e5882e73805b637a7284ac2
delberss <delberss@hotmail.com>
2025-05-27 12:58:53 -0300

    feature: add Products mock

---
+++ src/mock/productsMock.ts
@@ -0,0 +1,37 @@
+export const productsMock = [
+    {
+        id: 0,
+        title: "Playstation 5",
+        image: "https://gmedia.playstation.com/is/image/SIEPDC/ps5-product-thumbnail-01-en-14sep21?$facebook$",
+        description: "Console Sony Playstation 5 Digital",
+        price: "R$ 3.599"
+    },
+    {
+        id: 1,
+        title: "Notebook Dell",
+        image: "https://i.dell.com/is/image/DellContent/content/dam/ss2/product-images/dell-client-products/notebooks/inspiron-notebooks/15-3520/media-gallery/notebook-inspiron-15-3520-black-gallery-10.psd?fmt=png-alpha&pscan=auto&scl=1&hei=804&wid=1212&qlt=100,1&resMode=sharp2&size=1212,804&chrss=full",
+        description: "Notebook Dell Inspiron 15",
+        price: "R$ 4.299"
+    },
+    {
+        id: 2,
+        title: "iPhone 14",
+        image: "https://www.apple.com/newsroom/images/product/iphone/geo/Apple-iPhone-14-iPhone-14-Plus-2up-starlight-220907-geo_inline.jpg.large.jpg",
+        description: "Apple iPhone 14 128GB",
+        price: "R$ 5.999"
+    },
+    {
+        id: 3,
+        title: "Mouse Gamer",
+        image: "https://m.media-amazon.com/images/I/61FnLJfIU6L.jpg",
+        description: "Mouse Redragon Cobra Chroma RGB",
+        price: "R$ 199"
+    },
+    {
+        id: 4,
+        title: "Notebook Dell",
+        image: "https://i.dell.com/is/image/DellContent/content/dam/ss2/product-images/dell-client-products/notebooks/inspiron-notebooks/15-3520/media-gallery/notebook-inspiron-15-3520-black-gallery-10.psd?fmt=png-alpha&pscan=auto&scl=1&hei=804&wid=1212&qlt=100,1&resMode=sharp2&size=1212,804&chrss=full",
+        description: "Notebook Dell Inspiron 15",
+        price: "R$ 4.299"
+    },
+];

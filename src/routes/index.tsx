4c7191376096a3c24360609eb3fa4a141056bcc9
delberss <delberss@hotmail.com>
2025-05-27 13:00:40 -0300

    feature: add DefaultLayout in Route

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -4,6 +4,7 @@ import SignUp from '../pages/SignUp';
 import Home from '../pages/Home';
 import type { RootState } from '../store';
 import { useSelector } from 'react-redux';
+import { DefaultLayout } from '../layouts/DefaultLayout';
 
 
 interface PrivateRouteProps {
@@ -11,11 +12,11 @@ interface PrivateRouteProps {
 }
 
 const PrivateRoute: React.FC<PrivateRouteProps> = ({ children }) => {
-    const user = useSelector((state: RootState) => state.user);
-    if(!user.user){
-      return <Navigate to='/login' replace />
-    }
-    return children;
+  const user = useSelector((state: RootState) => state.user);
+  if (!user.user) {
+    return <Navigate to='/login' replace />
+  }
+  return children;
 }
 
 export function AppRoutes() {
@@ -23,13 +24,11 @@ export function AppRoutes() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path='/' element={
-          <PrivateRoute>
-            <Home/>
-          </PrivateRoute>
-        } />
         <Route path="/login" element={<Login />} />
         <Route path="/signup" element={<SignUp />} />
+        <Route element={<PrivateRoute><DefaultLayout /></PrivateRoute>}>
+          <Route path="/" element={<Home />} />
+        </Route>
       </Routes>
     </BrowserRouter>
   );

609ba6c1fd8e7d1cfed460045f322b5cf2dc85f5
delberss <delberss@hotmail.com>
2025-05-31 16:00:47 -0300

    fix: new mock

--- src/mock/productsMock.ts
+++ src/mock/productsMock.ts
@@ -1,37 +1,38 @@
 export const productsMock = [
-    {
-        id: 0,
-        title: "Playstation 5",
-        image: "https://gmedia.playstation.com/is/image/SIEPDC/ps5-product-thumbnail-01-en-14sep21?$facebook$",
-        description: "Console Sony Playstation 5 Digital",
-        price: "R$ 3.599"
-    },
-    {
-        id: 1,
-        title: "Notebook Dell",
-        image: "https://i.dell.com/is/image/DellContent/content/dam/ss2/product-images/dell-client-products/notebooks/inspiron-notebooks/15-3520/media-gallery/notebook-inspiron-15-3520-black-gallery-10.psd?fmt=png-alpha&pscan=auto&scl=1&hei=804&wid=1212&qlt=100,1&resMode=sharp2&size=1212,804&chrss=full",
-        description: "Notebook Dell Inspiron 15",
-        price: "R$ 4.299"
-    },
-    {
-        id: 2,
-        title: "iPhone 14",
-        image: "https://www.apple.com/newsroom/images/product/iphone/geo/Apple-iPhone-14-iPhone-14-Plus-2up-starlight-220907-geo_inline.jpg.large.jpg",
-        description: "Apple iPhone 14 128GB",
-        price: "R$ 5.999"
-    },
-    {
-        id: 3,
-        title: "Mouse Gamer",
-        image: "https://m.media-amazon.com/images/I/61FnLJfIU6L.jpg",
-        description: "Mouse Redragon Cobra Chroma RGB",
-        price: "R$ 199"
-    },
-    {
-        id: 4,
-        title: "Notebook Dell",
-        image: "https://i.dell.com/is/image/DellContent/content/dam/ss2/product-images/dell-client-products/notebooks/inspiron-notebooks/15-3520/media-gallery/notebook-inspiron-15-3520-black-gallery-10.psd?fmt=png-alpha&pscan=auto&scl=1&hei=804&wid=1212&qlt=100,1&resMode=sharp2&size=1212,804&chrss=full",
-        description: "Notebook Dell Inspiron 15",
-        price: "R$ 4.299"
-    },
+  {
+    id: "0",
+    slug: "playstation-5",
+    title: "Playstation 5",
+    image: "https://gmedia.playstation.com/is/image/SIEPDC/ps5-product-thumbnail-01-en-14sep21?$facebook$",
+    description: "Console Sony Playstation 5 Digital",
+    price: "3599",
+    details: "Inclui 1 controle DualSense, cabos HDMI e energia, garantia de 1 ano. Produto lacrado na caixa com suporte a jogos em 4K e Ray Tracing."
+  },
+  {
+    id: "1",
+    slug: "notebook-dell-inspiron-15",
+    title: "Notebook Dell",
+    image: "https://i.dell.com/is/image/DellContent/content/dam/ss2/product-images/dell-client-products/notebooks/inspiron-notebooks/15-3520/media-gallery/notebook-inspiron-15-3520-black-gallery-10.psd?fmt=png-alpha&pscan=auto&scl=1&hei=804&wid=1212&qlt=100,1&resMode=sharp2&size=1212,804&chrss=full",
+    description: "Notebook Dell Inspiron 15",
+    price: "4299",
+    details: "Intel Core i5 de 12ª geração, 8GB RAM, SSD de 256GB, tela Full HD de 15.6 polegadas, Windows 11 e bateria de longa duração. Ideal para estudos e trabalho."
+  },
+  {
+    id: "2",
+    slug: "iphone-14",
+    title: "iPhone 14",
+    image: "https://www.apple.com/newsroom/images/product/iphone/geo/Apple-iPhone-14-iPhone-14-Plus-2up-starlight-220907-geo_inline.jpg.large.jpg",
+    description: "Apple iPhone 14 128GB",
+    price: "5999",
+    details: "Tela Super Retina XDR de 6,1 polegadas, câmera dupla de 12MP, chip A15 Bionic, Face ID, resistência à água IP68 e sistema iOS atualizado."
+  },
+  {
+    id: "3",
+    slug: "mouse-gamer-redragon-cobra",
+    title: "Mouse Gamer",
+    image: "https://m.media-amazon.com/images/I/61FnLJfIU6L.jpg",
+    description: "Mouse Redragon Cobra Chroma RGB",
+    price: "199",
+    details: "Sensor de alta precisão, 7 botões programáveis, iluminação RGB personalizável, conexão USB e design ergonômico para longas sessões de jogo."
+  },
 ];
